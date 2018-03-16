478427e07de0012e44eca3ebc8be9ce95a456106
dudedean <aima10.aima11@gmail.com>
2018-03-16 17:33:06 +0800

    Users page working without photo

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','role_id','is_active','photo_id'
     ];
 
     /**
@@ -31,4 +31,11 @@ class User extends Authenticatable
     {
         return $this->belongsTo('App\Role');
     }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    
 }
